87bf9238645be9aa6e7e147cd6578b16db419397
Alexander Pajer <alexander.pajer@integrations.at>
2022-01-28 12:28:44 +0100

    dockerfile

--- Demos/07-Secure-Solutions/03-AppConnfig/Demo-01/food-api-appconfig/Startup.cs
+++ Demos/07-Secure-Solutions/03-AppConnfig/Demo-01/food-api-appconfig/Startup.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Azure.KeyVault;
 using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.EntityFrameworkCore;
